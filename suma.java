f1e18bf9699aa24db83a6d2a361368688b669f9e
Diego Fierro <diegoefierro@gmail.com>
2022-08-13 10:32:15 -0300

    Create suma.java

---
+++ suma.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 5;
+        int num3 = 15;
+        
+        suma(num1, num2, num3);
+    }
+    
+    public static void suma(int a, int b, int c){
+        System.out.println(a + b + c);
+    }
+}
